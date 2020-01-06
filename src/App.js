da11a9b90719cbd25ce081a1ca1dd28bdedb0fda
Anil Pai <anilbpai@gmail.com>
2020-01-06 17:27:00 -0600

    added 2 states

--- src/App.js
+++ src/App.js
@@ -1,12 +1,22 @@
 import React, {useState} from 'react'
 
 function App() {
-  const [count , setCount] = useState(10)
+  const [{count1, count2 }, setCount] = useState({ count1: 10, count2: 20})
 
   return (
     <div>
-      <button onClick={()=> setCount(currentCount => currentCount+1)}>+</button>
-      <div>{count}</div>
+      <button
+        onClick={() =>
+          setCount(currentState => ({
+            ...currentState,
+            count1: currentState.count1 + 1
+          }))
+        }
+      >
+        +
+      </button>
+      <div>{count1}</div>
+      <div>{count2}</div>
     </div>
   );
 }
