9b38de5a898b7aa64d755abf5eae96095252a80c
Anil Pai <anilbpai@gmail.com>
2020-01-07 16:15:38 -0600

    added custom forms

--- src/App.js
+++ src/App.js
@@ -1,21 +1,21 @@
-import React, {useState} from 'react'
+import React from 'react'
+import { useForm } from "./useForm"
 
 function App() {
-  const [email, setEmail] = useState("")
-  const [password, setPassword] = useState("")
+  const [values, handleChange] = useForm({email: '', password: ''})
 
   return (
     <div>
       <input
         name="email"
-        value={email}
-        onChange={e => setEmail(e.target.value)}
+        value={values.email}
+        onChange={handleChange}
       />
       <input
         type="password"
         name="password"
-        value={password}
-        onChange={e=> setPassword(e.target.value)}
+        value={values.password}
+        onChange={handleChange}
       />
     </div>
   );
