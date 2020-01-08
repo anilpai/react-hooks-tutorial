7666f3774f01610fa01b4fff37b0dc184a8cbff5
Anil Pai <anilbpai@gmail.com>
2020-01-07 18:44:48 -0600

    added +/- counters

--- src/App.js
+++ src/App.js
@@ -1,31 +1,29 @@
-import React from 'react'
+import React, {useState} from 'react'
 import { useForm } from "./useForm"
 import { useFetch } from "./useFetch"
 
 function App() {
   const [values, handleChange] = useForm({email: '', password: '', firstName: ''})
 
-  const {data, loading} = useFetch("http://numbersapi.com/43/trivia")
+  const [count, setCount] = useState(0)
+  const {data, loading} = useFetch(`http://numbersapi.com/${count}/trivia`)
 
   return (
     <>
-    <div>
-      {loading ? "loading......": data }
-    </div>
-    <div>
-      <input
-        name="email"
-        value={values.email}
-        onChange={handleChange}
-      />
-      <input
-        type="password"
-        name="password"
-        value={values.password}
-        onChange={handleChange}
-      />
-    </div>
-  </>
+      <div>{loading ? "loading......" : data}</div>
+      <div>count: {count}</div>
+      <button onClick={() => setCount(c => c + 1)}>+</button>
+      <button onClick={() => setCount(c => c - 1)}>-</button>
+      <div>
+        <input name="email" value={values.email} onChange={handleChange} />
+        <input
+          type="password"
+          name="password"
+          value={values.password}
+          onChange={handleChange}
+        />
+      </div>
+    </>
   );
 };
 
