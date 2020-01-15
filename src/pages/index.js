dd0ceafe612ea21bfb3d29cfed16df00f116af8a
Anil Pai <anilbpai@gmail.com>
2020-01-15 02:02:12 -0600

    useContext: fixed :bug:

--- src/pages/index.js
+++ src/pages/index.js
@@ -2,10 +2,11 @@ import React, { useContext } from "react";
 import { UserContext } from "../UserContext";
 
 export function Index() {
-  const msg = useContext(UserContext);
+  const { value } = useContext(UserContext);
   return (
     <div>
-      <h2>Home</h2>;<div>{msg}</div>
+      <h2>Home</h2>
+      <div>{value}</div>
     </div>
   );
 }
